a22289d8745fab995ed80d8478a20dc0c8ff8cd3
ZakL1 <zakerylyel32@gmail.com>
2025-04-05 09:47:49 +0100

    added crud functionality to the comment model

--- comments/urls.py
+++ comments/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from comments import views
+from .views import CommentList, CommentDetail
 
 urlpatterns = [
     path('comments/', views.CommentList.as_view()),
+    path('comments/<int:pk>/', CommentDetail.as_view(), name='comment-detail'),
 ]
--- comments/views.py
+++ comments/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics, permissions
 from comments.models import Comment
 from comments.serializers import CommentSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
 
 
 class CommentList(generics.ListCreateAPIView):
@@ -13,3 +16,16 @@ class CommentList(generics.ListCreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveDestroyAPIView):
+    "Retrieve or delete a comment"
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+
+    def delete(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.owner != request.user:
+            return Response({'detail': 'Not allowed'}, status=status.HTTP_403_FORBIDDEN)
+        return super().delete(request, *args, **kwargs)
