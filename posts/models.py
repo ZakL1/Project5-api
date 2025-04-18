a887e0236f17cb99bf1e22ab35cd5e1f8b727294
ZakL1 <zakerylyel32@gmail.com>
2025-04-18 18:13:40 +0100

    add image sizing to post model

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
 
 class Post(models.Model):
     """
@@ -20,3 +22,24 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.id} {self.title}'
+
+    def save(self, *args, **kwargs):
+        # Call original save method to ensure we have an image file
+        super().save(*args, **kwargs)
+
+        if self.image:
+            img = Image.open(self.image)
+            output = BytesIO()
+
+            # Resize the image (change dimensions here)
+            img = img.convert('RGB')  # In case image is RGBA or other mode
+            img = img.resize((800, 600), Image.ANTIALIAS)
+
+            # Save to BytesIO object
+            img.save(output, format='JPEG', quality=85)
+            output.seek(0)
+
+            # Save resized image back to the image field
+            self.image.save(self.image.name, ContentFile(output.read()), save=False)
+            super().save(*args, **kwargs)  # Save again after resizing
+            img.save(self.image.path, format='JPEG', quality=85)
