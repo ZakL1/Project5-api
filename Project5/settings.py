266d842740474c90e6755bd1392b55c324d504e3
ZakL1 <zakerylyel32@gmail.com>
2025-04-30 22:38:54 +0100

    fixed database and secret key in settings

--- Project5/settings.py
+++ Project5/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
 import os
 import sys
 import dj_database_url
@@ -33,13 +32,16 @@ STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['shutter-api-aad07b464590.herokuapp.com']
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://shutter-main-990a60fdcc03.herokuapp.com"
+]
 
 # Application definition
 
@@ -86,7 +88,6 @@ CORS_ALLOWED_ORIGINS = [
     "https://shutter-main-990a60fdcc03.herokuapp.com"
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'Project5.urls'
@@ -122,14 +123,17 @@ WSGI_APPLICATION = 'Project5.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
-}
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
