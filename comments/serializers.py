7350899250ca7ff8a317af6275470466910b3b93
ZakL1 <zakerylyel32@gmail.com>
2025-05-01 20:15:18 +0100

    updated comments serialzier to allow editing comments

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,23 +1,19 @@
 from rest_framework import serializers
-from .models import Post
 from comments.models import Comment
-
 
 class CommentSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    post = serializers.ReadOnlyField() 
 
     class Meta:
         model = Comment
-        fields = [
-            'id', 'owner', 'created_at',
-            'body', 'post'            
-        ]
+        fields = ['id', 'owner', 'created_at', 'body', 'post']
 
-def validate_body(self, value):
-    value = value.strip()
-    if not value:
-        raise serializers.ValidationError("Comment cannot be empty.")
-    if len(value) > 100:
-        raise serializers.ValidationError("Comment must be less than 100 characters long.")
-    return value
+    def validate_body(self, value):
+        value = value.strip()
+        if not value:
+            raise serializers.ValidationError("Comment cannot be empty.")
+        if len(value) > 100:
+            raise serializers.ValidationError("Comment must be less than 100 characters long.")
+        return value
 
