2e43a9a66cfc8fc4f6b0cf491deceeaafc5a1294
ZakL1 <zakerylyel32@gmail.com>
2025-06-17 20:15:03 +0100

    updated challenge serializer to posts are ordered by like count

--- challenges/serializers.py
+++ challenges/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Challenge
 from posts.models import Post
 from posts.serializers import PostSerializer
+from django.db.models import Count
 
 class ChallengeSerializer(serializers.ModelSerializer):
     top_posts = serializers.SerializerMethodField()
@@ -10,6 +11,9 @@ class ChallengeSerializer(serializers.ModelSerializer):
         model = Challenge
         fields = ['id', 'title', 'description', 'start_date', 'end_date', 'top_posts']
 
-    def get_top_posts(self, obj):
-        top = Post.objects.filter(challenge=obj).order_by('-likes_count')[:3]
-        return PostSerializer(top, many=True, context=self.context).data
+def get_top_posts(self, obj):
+    top = Post.objects.filter(challenge=obj) \
+        .annotate(likes_count=Count('likes', distinct=True)) \
+        .order_by('-likes_count')[:3]
+    return PostSerializer(top, many=True, context=self.context).data
+
